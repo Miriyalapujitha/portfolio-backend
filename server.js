e7ce070c032433e9dd0ecc18a0962064d522997c
Pujitha <pujithamiriyala418@gmail.com>
2026-05-28 18:23:33 +0530

    force ipv4 for gmail smtp

--- server.js
+++ server.js
@@ -50,6 +50,10 @@ app.get("/", (req, res) => {
 // ----------------------
 const transporter = nodemailer.createTransport({
   service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  family: 4, // FORCE IPV4
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
