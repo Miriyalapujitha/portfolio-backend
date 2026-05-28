ef7e781aec7c35bdc3f3981124b5660d4a681f9f
Pujitha <pujithamiriyala418@gmail.com>
2026-05-28 18:30:58 +0530

    final smtp ipv4 fix

--- server.js
+++ server.js
@@ -48,15 +48,19 @@ app.get("/", (req, res) => {
 // ----------------------
 // Email Transporter
 // ----------------------
+import nodemailer from "nodemailer"
+
 const transporter = nodemailer.createTransport({
-  service: "gmail",
   host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
-  family: 4, // FORCE IPV4
+  port: 587,
+  secure: false,
+  family: 4,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false,
   },
 })
 
